8c1b4f9e20c79cbef91ba87ddec1aace54eb2a77
Akshay Yewle <akshayyewle@gmail.com>
2024-02-18 23:43:17 +0000

    Commit

--- 10 Local LLM App/app04 - Youtube Title & Script Assistant V2.py
+++ 10 Local LLM App/app04 - Youtube Title & Script Assistant V2.py
@@ -2,11 +2,10 @@
 
 import os
 import streamlit as st
-# import openai , langchain, wikipedia, chromadb
+import openai , langchain, wikipedia, chromadb
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
-from langchain.memory import ConversationBufferMemory
 
 # Title
 st.title("YouTube Title & Script Assistant 📺")
@@ -17,17 +16,12 @@ title_template = PromptTemplate(input_variables=['topic'],
 script_template = PromptTemplate(input_variables=['title'],
                                 template='Suggest A Youtube Video Script About {title}')
 
-# Memory
-Memory01 = ConversationBufferMemory(input_key='topic',memory_key='chat_history')
-
 # Create LLM Connection
 # C:/Users/aksha/AppData/Local/nomic.ai/GPT4All/
 api_key = os.getenv('OpenAI_API_Key') 
 LLM = OpenAI(temperature=0.9)
-Title_Chain = LLMChain(llm=LLM,prompt=title_template,verbose=True,
-                       output_key='title',memory=Memory01)
-Script_Chain = LLMChain(llm=LLM,prompt=script_template,verbose=True,output_key='script',
-                        memory=Memory01)
+Title_Chain = LLMChain(llm=LLM,prompt=title_template,verbose=True,output_key='title')
+Script_Chain = LLMChain(llm=LLM,prompt=script_template,verbose=True,output_key='script')
 Sequential_Chain01 = SequentialChain(chains=[Title_Chain,Script_Chain],
                  input_variables=['topic'],
                  output_variables=['title','script'],
